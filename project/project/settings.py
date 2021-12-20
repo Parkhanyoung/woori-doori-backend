1e0cbf98baf4155479c6dfe1e23d3cd9857dc4eb
hanyoung <phanyoung@naver.com>
2021-12-21 00:15:47 +0900

    Fix: cors 미들웨어 추가

--- project/project/settings.py
+++ project/project/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -172,7 +173,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = [
-#  'http://localhost:3000',
-#  'http://127.0.0.1:3000',
-# ]
+CORS_ORIGIN_WHITELIST = [
+ 'http://localhost:3000',
+ 'http://127.0.0.1:3000',
+]
