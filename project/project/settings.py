21785bcc04db054c470ae21677615f3de1ecba9d
HyoRyoung-Yang <js980303@naver.com>
2021-12-21 01:02:33 +0900

    버그: cors 에러 해결

--- project/project/settings.py
+++ project/project/settings.py
@@ -172,8 +172,10 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
  'http://localhost:3000',
  'http://127.0.0.1:3000',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
