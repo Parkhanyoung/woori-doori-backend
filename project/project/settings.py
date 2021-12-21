cc75faa57dbcb94a3331dd3fefb036f660a1b3b2
hanyoung <phanyoung@naver.com>
2021-12-21 17:28:37 +0900

    settings.py 사소한 변화

--- project/project/settings.py
+++ project/project/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-PARKHANYOUNG = True
+PARKHANYOUNG = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
